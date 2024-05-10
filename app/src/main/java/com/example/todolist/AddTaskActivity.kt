3d09d60fe4786e7ea92cc1c3323a2249d57c1b21
SithagiV <153867463+SithagiV@users.noreply.github.com>
2024-05-10 13:53:28 +0530

    s

--- app/src/main/java/com/example/todolist/AddTaskActivity.kt
+++ app/src/main/java/com/example/todolist/AddTaskActivity.kt
@@ -2,10 +2,27 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.todolist.databinding.ActivityAddTaskBinding
+import com.example.todolist.databinding.ActivityMainBinding
 
 class AddTaskActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddTaskBinding
+    private lateinit var db:TaskDatabesehelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_task)
+        binding = ActivityAddTaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = TaskDatabesehelper(this)
+
+        binding.saveButton.setOnClickListener{
+            val content = binding.task.text.toString()
+            val task = Task(0,content)
+            db.insertTask(task)
+            finish()  //will do the same thing as intenet 
+            Toast.makeText(this,"Task Saved",Toast.LENGTH_SHORT).show()
+        }
     }
 }
